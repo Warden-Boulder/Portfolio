8d63e3c17111bae26fb40034f148e7d2ec22d871
Boulder <157924017+Warden-Boulder@users.noreply.github.com>
2026-02-01 23:41:34 -0700

    Refactor personal function for display updates

--- script.js
+++ script.js
@@ -1,34 +1,63 @@
 function personal() {
   let personalSelect = document.querySelector('select[onchange="personal()"]');
   let personal = personalSelect.value;
-  let display = document.getElementById('display-one');
-  display.className = "display-one";
+  let display = document.getElementById('personal-display');
+  display.className = "personal-display";
   let text = "";
   
   if (personal === "showcase") {
-    text = `<strong><em>Vault Hunter Toolbox</strong><br>
-      <strong class="underline">Tech Stack:</strong> HTML, CSS, JavaScript<br>
-      <strong class="underline">Description:</strong> A web tool to calculate skill cooldowns for characters in the borderlands series</em><br>
+    text = `<strong class="silverback"><em>Vault Hunter's Toolbox</strong><br>
+      <strong class="tech">Tech Stack:</strong> 
+      <ol class="tech-li">
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+      </ol>
+      <p class="silverback"><strong class="des">Description:</strong><br>
+      A web tool to assist any borderlands player in calculating the total time their action skill will be in cooldown for as well as be able to calculate the total time it'll take their shields or health to reach 100% capacity.</em></p>
     <a href="https://warden-boulder.github.io/Borderlands-2-Cooldown-Website/" target="_blank">Live Site</a>`;
-  } else if (personal === "sites") {
-    text = `<em><strong>Inkweaver and Codewright Portfolio Website</strong><br>
-    <strong class="underline">Tech Stack:</strong> HTML and CSS<br>
-    <strong class="underline">Description:</strong> Built the current website to display the translation and developer projects I've worked on or developed.</em><br><br>
-    <em><strong>Warden's Outpost</strong><br>
-    <strong class="underline">Tech Stack:</strong> HTML and CSS<br>
-    <strong class="underline">Description:</strong€ A website I built to act as my commissions site.</em><br>
-    <a href="https://warden-boulder.github.io/Commissions/" target="_blank">Live Site</a>`;
-  } else if (personal === "tools") {
-    text = `<em><strong>Unit Conversion</strong><br>
-    <strong class="underline">Tech Stack:</strong> HTML, CSS, JavaScript<br>
-    <strong class="underline">Description:</strong> A website I built to convert various units of measurements that include length, mass, speed, and temperature to another of the same kind of unit.</em><br>
+  } else if (personal === "portfolio") {
+    text = `<em><strong class="silverback">Personal Portfolio Site</strong><br>
+    <strong class="tech">Tech Stack:</strong> 
+    <ol class="tech-li">
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>JavaScript</li>
+    </ol>
+    <p class="silverback"><strong class="des">Description:</strong><br>
+    Built and designed the current website to display both my translation work and developer projects I've either worked on and/or developed.<p></em>`;
+  } else if (personal === "commission") {
+    text = `<em><strong class="silverback">Personal Commission Site</strong><br>
+    <strong class="tech">Tech Stack:</strong> 
+    <ol class="tech-li">
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>JavaScript</li>
+    </ol>
+    <p class="silverback"><strong class="des">Description:</strong><br>
+    Built and designed to act as a go to place for the services that I offer ranging from translations services to web development services.<p></em>
+    <a href="https://warden-boulder.github.io/Commission/index.html">Live Site</a>`;
+  } else if (personal === "convert") {
+    text = `<em><strong class="silverback">Unit Converter</strong><br>
+    <strong class="tech">Tech Stack:</strong> 
+    <ol class="tech-li">
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>JavaScript</li>
+    </ol>
+    <p class="silverback"><strong class="des">Description:</strong><br>A conversion tool to help convert measurements between various unit categories that include units like length, mass, speed, and temperature.</p></em><br>
     <a href="https://warden-boulder.github.io/Converter/" target="_blank">Live Site</a>`;
-  } else if (personal === "challenges") {
-    text = `<em><strong>FizzBuzz and Palindrome Challenges</strong><br>
-    <strong class="underline">Tech Stack:</strong> JavaScript and Python<br>
-    <strong class="underline">Description:</strong> These are the only 2 coding challenges I've done to date. My take on both using a mix of Python and JavaScript can be found on my GitHub, linked below.</em><br>
+  } else if (personal === "challenge") {
+    text = `<em><strong class="silverback">Various Challenges</strong><br>
+    <strong class="tech">Tech Stack:</strong> 
+    <ol class="tech-li">
+    <li>JavaScript</li>
+    <li>Python</li>
+    </ol>
+    <p class="silverback"><strong class="des">Description:</strong><br>
+    The only 2 challenges that I've done thus far are the fizzbuzz and palindrome coding challenges. Both linked to my GitHub below.</p></em><br>
     <a href="https://github.com/Warden-Boulder/Coding-Challenge/tree/main" target="_blank">GitHub</a>`;
-  }
+  } 
   
   display.innerHTML = text;
 }
@@ -41,14 +70,16 @@ function professional() {
   let text = "";
   
   if (pro === "aviation") {
-    text = `<em><strong>B737 Horizontal Freeplay Calculator</strong><br>
-    <strong class="underline">Type:</strong> Job Efficiency Tool<br>
-    <strong class="underline">Tech Stack:</strong> HTML, CSS, JavaScript, Python<br>
-    <strong class="underline">Description:</strong> Both a web tool and python based tools I developed, with the aid of coworkers to ensure accuracy, to aid myself and coworkers to help verify that the results of a horizontal freeplay check that is performed on both Boeing 737NG and 737 Max aircraft are within or out limits in accordance with all appropriate maintenance manuals and airworthiness directives (ADs).</em><br><br>
-    Links:<br>
-    <a href="https://warden-boulder.github.io/B737-Horizontal-Freeplay-Website/" target="_blank">Live Site</a><br>
-    <a href="https://github.com/Warden-Boulder/B737NG-Horizontal-Freeplay-Calculator" target="_blank">B737NG Python Code</a><br>
-    <a href="https://github.com/Warden-Boulder/B737Max-Horizontal-Freeplay-Calculator" target="_blank">B737 Max Python Code</a>`;
+    text = `<em><strong class="amethyst">Aviation Maintenance Toolbox</strong><br>
+    <strong class="amethyst underline"><em class="amethyst">Build Type:</strong> Job Efficiency Tool</em><br>
+    <strong class="tech2">Tech Stack:</strong> 
+    <ol class="tech-li2">
+    <li>JavaScript</li>
+    <li>Python</li>
+    </ol>
+    <p class="amethyst"><strong class="des2">Description:</strong><br> Both a web tool and python based tool I developed, with the aid of coworkers to ensure accuracy, to aid myself and coworkers to help verify that the results of a horizontal freeplay check that is performed on both Boeing 737NG and 737 Max aircraft are within or out limits in accordance with all appropriate maintenance manuals and airworthiness directives (ADs).</p></em>
+    <a href="https://warden-boulder.github.io/Aviation-Maintenance-Toolbox/" target="_blank">Live Site</a><br>
+    <a href="https://github.com/Warden-Boulder/B737-Horizontal-Freeplay/tree/main">Python Code</a>`;
   }
   
   display.innerHTML = text;
